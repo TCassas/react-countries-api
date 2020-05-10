6624c4dd13a60a76046412486855e2e2c1c6f8b1
Tomás Cassas <tomascassas@gmail.com>
2020-05-10 03:30:32 -0300

    Updates

---
+++ precache-manifest.7dcb15dccaa888c2b519fdee43f88245.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d57153f668a8c4499095a79bce0dd92e",
+    "url": "/react-countries-api/index.html"
+  },
+  {
+    "revision": "bdd3dc00109efb1021bf",
+    "url": "/react-countries-api/static/css/main.421e727f.chunk.css"
+  },
+  {
+    "revision": "93e3eeaacd201d0d952f",
+    "url": "/react-countries-api/static/js/2.0a270dee.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-countries-api/static/js/2.0a270dee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bdd3dc00109efb1021bf",
+    "url": "/react-countries-api/static/js/main.da54c383.chunk.js"
+  },
+  {
+    "revision": "892cdaff53dcb26c0f95",
+    "url": "/react-countries-api/static/js/runtime-main.09c9957a.js"
+  }
+]);
